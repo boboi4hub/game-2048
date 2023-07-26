0b0011fa6c64b4645bbb063ea358199dc35ed9e5
oriabukha <ale_rya1@hotmail.com>
2023-07-26 19:14:01 +0200

    Make logic playable.

--- OneZeroFourEight/base.js
+++ OneZeroFourEight/base.js
@@ -1,123 +1,143 @@
+/// UTILS
+
 function merge(x1, x2) {
-    if (x1 === x2) {
-        return x1 * 2;
-    } else {
-        return x1;
-    }
+  return x1 === x2 ? x1 * 2 : x1;
 }
 
 function mergeArray(xs) {
-    const filtered = xs.filter((n) => n != 0);
-    const result = [];
+  const filtered = xs.filter(n => n != 0);
+  const result = [];
 
-    for (let i = 0; i < filtered.length; i++) {
-        const m = merge(filtered[i], filtered[i + 1]);
-        result.push(m);
-        if (m !== filtered[i]) {
-            i++;
-        }
+  for (let i = 0; i < filtered.length; i++) {
+    const m = merge(filtered[i], filtered[i + 1]);
+    result.push(m);
+    if (m !== filtered[i]) {
+      i++;
     }
+  }
 
-    return [...result, ...[0, 0, 0, 0]].slice(0, xs.length);
+  return [...result, ...[0, 0, 0, 0]].slice(0, xs.length);
 }
 
 function mergeMatrix(xss) {
-    const result = [];
-    for (let i = 0; i < xss.length; i++) {
-        //    console.log(mergeArray (xss[i]))
-        result.push(mergeArray(xss[i]));
-    }
-    return result;
+  const result = [];
+  for (let i = 0; i < xss.length; i++) {
+    result.push(mergeArray(xss[i]));
+  }
+  return result;
 }
 
 function copyMatrix(matrix) {
-    const n = matrix.length;
-    const copiedMatrix = [];
+  const n = matrix.length;
+  const copiedMatrix = [];
 
-    for (let i = 0; i < n; i++) {
-        copiedMatrix.push([...matrix[i]]);
-    }
+  for (let i = 0; i < n; i++) {
+    copiedMatrix.push([...matrix[i]]);
+  }
 
-    return copiedMatrix;
+  return copiedMatrix;
 }
 
 function rotateMatrixRight(matrix) {
-    const result = copyMatrix(matrix);
-    const n = matrix.length;
+  const result = copyMatrix(matrix);
+  const n = matrix.length;
 
-    // Transpose the matrix
-    for (let i = 0; i < n; i++) {
-        for (let j = i + 1; j < n; j++) {
-            // Swap matrix[i][j] with matrix[j][i]
-            const temp = result[i][j];
-            result[i][j] = result[j][i];
-            result[j][i] = temp;
-        }
+  // Transpose the matrix
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      // Swap matrix[i][j] with matrix[j][i]
+      const temp = result[i][j];
+      result[i][j] = result[j][i];
+      result[j][i] = temp;
     }
+  }
 
-    // Reverse each row
-    for (let i = 0; i < n; i++) {
-        result[i] = result[i].reverse();
-    }
+  // Reverse each row
+  for (let i = 0; i < n; i++) {
+    result[i] = result[i].reverse();
+  }
 
-    return result;
+  return result;
 }
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  const min1 = Math.ceil(min);
+  const max1 = Math.floor(max);
+  return Math.floor(Math.random() * (max1 - min1 + 1)) + min1;
 }
 
 function findCoordinates(matrixSize, number) {
-    const n = Math.sqrt(matrixSize);
-    const row = Math.floor(number / n);
-    const column = number % n;
-    return [row, column];
+  const n = Math.sqrt(matrixSize);
+  const row = Math.floor(number / n);
+  const column = number % n;
+  return [row, column];
 }
 
-////// API //////
+function emptyCoordinates(xss) {
+  const result = [];
+  for (let i = 0; i < xss.length; i++) {
+    for (let j = 0; j < xss.length; j++) {
+      if (xss[i][j] === 0) {
+        result.push([i, j]);
+      }
+    }
+  }
+  return result;
+}
 
-function up(xss) {
+const API = {
+  up: xss => {
     const r = rotateMatrixRight(rotateMatrixRight(rotateMatrixRight(xss)));
     const result = mergeMatrix(r);
     return rotateMatrixRight(result);
-}
-function down(xss) {
+  },
+  down: xss => {
     const r = rotateMatrixRight(xss);
     const result = mergeMatrix(r);
     return rotateMatrixRight(rotateMatrixRight(rotateMatrixRight(result)));
-}
-function left(xss) {
+  },
+  left: xss => {
     return mergeMatrix(xss);
-}
-function right(xss) {
+  },
+  right: xss => {
     const r = rotateMatrixRight(rotateMatrixRight(xss));
     const result = mergeMatrix(r);
     return rotateMatrixRight(rotateMatrixRight(result));
-}
-
-function init(n) {
+  },
+  init: n => {
     const rand = () => getRandomInt(0, n * n - 1);
     let x = [rand(), rand()];
     while (x[0] === x[1]) {
-        x = [rand(), rand()];
+      x = [rand(), rand()];
     }
     const result = [];
 
     for (let i = 0; i < n; i++) {
-        result[i] = [];
-        for (let j = 0; j < n; j++) {
-            result[i][j] = 0;
-        }
+      result[i] = [];
+      for (let j = 0; j < n; j++) {
+        result[i][j] = 0;
+      }
     }
 
+    const a = findCoordinates(n * n, x[0]);
+    const b = findCoordinates(n * n, x[1]);
 
-    const a = findCoordinates(n*n, x[0])
-    const b = findCoordinates(n*n, x[1])
-
-    result[a[0]][a[1]] = 2
-    result[b[0]][b[1]] = 2
+    result[a[0]][a[1]] = 2;
+    result[b[0]][b[1]] = 2;
 
     return result;
-}
+  },
+  addRandom: xss => {
+    const copy = copyMatrix(xss);
+    const emptyCells = emptyCoordinates(copy);
+
+    if (emptyCells.length === 0) {
+      return undefined;
+    } else {
+      const randomIndex = getRandomInt(0, emptyCells.length - 1);
+      const [x, y] = emptyCells[randomIndex];
+      copy[x][y] = 2;
+      return copy;
+    }
+  },
+};
